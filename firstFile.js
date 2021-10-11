6d67f99d9b860c4fab3c6144d3613f7df61eaae3
Dan Ichim <danichim.work@gmail.com>
2021-10-11 14:26:32 +0300

    Add files via upload

---
+++ firstFile.js
@@ -0,0 +1,3 @@
+let hello = "hello world!";
+
+global.console.log(hello);
---
+++ global.js
@@ -0,0 +1,6 @@
+console.log(__dirname);
+console.log(__filename);
+
+const path = require('path');
+
+console.log(`The filname is ${path.basename(__filename)}`);
